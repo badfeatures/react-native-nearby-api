01cbf088d21c2a9e678d416411cff6ad2bb93596
Mark Jackson <markjacks205@gmail.com>
2017-10-31 15:31:10 -0700

    Updated example app with new API

--- example/App.js
+++ example/App.js
@@ -4,56 +4,83 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
-import { 
-  NearbyAPI
-} from 'react-native-nearby-api'
+  View,
+  TouchableOpacity
+} from "react-native";
+import { NearbyAPI } from "react-native-nearby-api";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-export default class App extends Component<{}> {
+const nearbyAPI = new NearbyAPI();
+
+export default class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
+      isConnected: false,
       nearbyMessage: null
-    }
+    };
   }
-  
+
   componentDidMount() {
-    console.log('Mounting ', NearbyAPI)
-    const nearbyAPI = new NearbyAPI()
+    console.log("Mounting ", NearbyAPI);
     nearbyAPI.onConnected(message => {
-      console.log(message)
-      this.setState({ nearbyMessage: `Connected - ${message}` })
-    })
+      console.log(message);
+      this.setState({
+        isConnected: true,
+        nearbyMessage: `Connected - ${message}`
+      });
+    });
     nearbyAPI.onFound(message => {
-      console.log('Message Found!')
-      console.log(message)
-      this.setState({ nearbyMessage: `Message Found - ${message}` })
-    })
+      console.log("Message Found!");
+      console.log(message);
+      this.setState({ nearbyMessage: `Message Found - ${message}` });
+    });
     nearbyAPI.onLost(message => {
-      console.log('Message Lost!')
-      console.log(message)
-      this.setState({ nearbyMessage: `Message Lost - ${message}` })
-    })
+      console.log("Message Lost!");
+      console.log(message);
+      this.setState({ nearbyMessage: `Message Lost - ${message}` });
+    });
     nearbyAPI.onDistanceChanged((message, value) => {
-      console.log('Distance Changed!')
-      console.log(message, value)
-      this.setState({ nearbyMessage: `Distance Changed - ${message} - ${value}` })
-    })
-    nearbyAPI.publish(`Hello World! - ${Math.random()}`)
+      console.log("Distance Changed!");
+      console.log(message, value);
+      this.setState({
+        nearbyMessage: `Distance Changed - ${message} - ${value}`
+      });
+    });
+    nearbyAPI.onPublishSuccess(message => {
+      console.log(message);
+      this.setState({ nearbyMessage: `Publish Success - ${message}` });
+    });
+    nearbyAPI.onPublishFailed(message => {
+      console.log(message);
+      this.setState({ nearbyMessage: `Publish Failed - ${message}` });
+    });
+    nearbyAPI.onSubscribeSuccess(() => {
+      this.setState({ nearbyMessage: `Subscribe Success` });
+    });
+    nearbyAPI.onSubscribeFailed(() => {
+      this.setState({ nearbyMessage: `Subscribe Failed` });
+    });
   }
+
+  _connectPress = () => {
+    nearbyAPI.connect();
+  };
+
+  _publishPress = () => {
+    nearbyAPI.publish(`Hello World! - ${Math.random()}`);
+  };
 
   render() {
     return (
@@ -62,8 +89,16 @@ export default class App extends Component<{}> {
           Welcome to react-native-nearby-api test app!
         </Text>
         <Text style={styles.instructions}>
-         {this.state.nearbyMessage}
+          Is Connected: {`${this.state.isConnected}`}
         </Text>
+        <Text style={styles.instructions}>{this.state.nearbyMessage}</Text>
+        <TouchableOpacity onPress={this._connectPress}>
+          <Text style={styles.connectButton}>CONNECT</Text>
+        </TouchableOpacity>
+        <View style={{ height: 32 }} />
+        <TouchableOpacity onPress={this._publishPress}>
+          <Text style={styles.connectButton}>PUBLISH</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -72,18 +107,23 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
+  },
+  connectButton: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
